a016022e5a9e777d16173a14975f8642f5fa9cbd
caijinglong-b-imac <cjl_spy@163.com>
2020-10-30 17:27:19 +0800

    Add document.

--- lib/src/dependency.dart
+++ lib/src/dependency.dart
@@ -1,15 +1,22 @@
+import 'package:meta/meta.dart';
+
 import 'project_base.dart';
 
+/// Describe dependency.
 class Dependency {
-  final Package rootPackage;
-
+  /// Use [rootPackage] to create [Dependency] object.
   Dependency({
     this.rootPackage,
   });
 
+  /// The result may be null.
+  ///
+  /// Must rootPackage.
+  ///
+  /// The [object] is the yaml.
   factory Dependency.fromYaml({
-    Package rootPackage,
-    object,
+    @required Package rootPackage,
+    @required object,
   }) {
     if (object is MapEntry) {
       return Dependency.fromMapEntry(
@@ -21,9 +28,10 @@ class Dependency {
     return null;
   }
 
+  /// Using the dependency node to create [Dependency] object.
   factory Dependency.fromMapEntry({
-    Package rootPackage,
-    MapEntry entry,
+    @required Package rootPackage,
+    @required MapEntry entry,
   }) {
     final dep = Dependency(
       rootPackage: rootPackage,
@@ -34,6 +42,12 @@ class Dependency {
     return dep;
   }
 
+  /// The root package.
+  final Package rootPackage;
+
+  /// Convert type value of yaml to the [DependencyType].
+  ///
+  /// If it is null, there is a problem. Please submit an issue.
   static DependencyType getType(value) {
     if (value is String) {
       return DependencyType.pub;
@@ -52,16 +66,21 @@ class Dependency {
 
   String _name;
 
+  /// The name of the dependency.
   String get name {
     return _name;
   }
 
   DependencyType _type;
 
+  /// The type of the dependency.
   DependencyType get type {
     return _type;
   }
 
+  /// The package of the dependency.
+  ///
+  /// It be found with name by the `.package` file of root package.
   Package get package {
     try {
       final packageInfo = rootPackage.packageFileInfo[name];
@@ -75,12 +94,21 @@ class Dependency {
     }
   }
 
+  /// The version of the dependency.
   String get version => package.version;
 }
 
+/// Type of the dependency.
 enum DependencyType {
+  /// The package come from [pub](https://pub.dev).
   pub,
+
+  /// Local packages.
   path,
+
+  /// The package is from git.
   git,
+
+  /// The package come from sdk(flutter or dart).
   sdk,
 }
